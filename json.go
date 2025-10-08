250a06bf0e0f5234373e20fc0b73a0cafc94c0d2
Chukwuemeka <mekastans@gmail.com>
2025-10-08 03:04:46 +0100

    Added security checks to CI workflow

--- json.go
+++ json.go
@@ -30,5 +30,9 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	// Handle the write error
+    _, err = w.Write(dat)
+    if err != nil {
+        log.Printf("Error writing response: %s", err)
+    }
 }
